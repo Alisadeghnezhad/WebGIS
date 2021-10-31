39b359ecbd7770361c8535eb6f44e1c0f9827c40
Alisadeghnezhad <alisadeghnezhad80@gmail.com>
2021-10-31 22:57:47 +0330

    First_WebGIS

---
+++ data/amenity_car_wash_4.js
@@ -0,0 +1,8 @@
+var json_amenity_car_wash_4 = {
+"type": "FeatureCollection",
+"name": "amenity_car_wash_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "full_id": "n4448763091", "osm_id": "4448763091", "osm_type": "node", "amenity": "car_wash", "name_en": "omid carwash" }, "geometry": { "type": "Point", "coordinates": [ 51.3965789, 35.7691329 ] } }
+]
+}
---
+++ data/amenity_university_2.js
@@ -0,0 +1,9 @@
+var json_amenity_university_2 = {
+"type": "FeatureCollection",
+"name": "amenity_university_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "full_id": "n5764546773", "osm_id": "5764546773", "osm_type": "node", "amenity": "university", "name": "دانشگاه علوم پزشکی ایران" }, "geometry": { "type": "Point", "coordinates": [ 51.408498, 35.7684232 ] } },
+{ "type": "Feature", "properties": { "full_id": "n6865486375", "osm_id": "6865486375", "osm_type": "node", "amenity": "university", "name": "بخش اداری دانشگاه آزاد اسلامی واحد غرب" }, "geometry": { "type": "Point", "coordinates": [ 51.4431869, 35.7699962 ] } }
+]
+}
